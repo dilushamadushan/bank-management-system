d1f0c9b01e5896b2e03755d2da507853bdfcdd60
dilushamadushan <dmdmadushan007@gmail.com>
2025-07-28 21:42:55 +0530

    created register form

--- src/main/java/com/bank/controller/LoginController.java
+++ src/main/java/com/bank/controller/LoginController.java
@@ -2,10 +2,22 @@ package com.bank.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+
+    @FXML
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
 
     @FXML
     private TextField password;
@@ -19,8 +31,21 @@ public class LoginController {
     }
 
     @FXML
-    void register(MouseEvent event) {
+    private void register(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/bank/views/register.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Register");
+            stage.setScene(new Scene(root));
+            stage.show();
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
---
+++ src/main/java/com/bank/controller/RegisterController.java
@@ -0,0 +1,41 @@
+package com.bank.controller;
+
+import com.bank.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterController {
+
+    @FXML
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
+
+    @FXML
+    void loginCon(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/login.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Main!");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void register(ActionEvent event) {
+        System.out.println("Btn Clicked");
+    }
+
+}
+
