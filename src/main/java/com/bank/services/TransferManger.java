f84c831cdf2cb69e111052152733a1f86550d8ca
dilushamadushan <dmdmadushan007@gmail.com>
2025-08-13 01:02:36 +0530

    handled trasaction prosess

---
+++ src/main/java/com/bank/services/TransferManger.java
@@ -0,0 +1,36 @@
+package com.bank.services;
+
+import com.bank.models.Account;
+import com.bank.models.Transfer;
+import java.sql.SQLException;
+
+public class TransferManger {
+    private AccountManager acm = new AccountManager();
+
+    public void transfer(Transfer transfer,String username) throws SQLException {
+        String _fromACC = transfer.get_fromAcc();
+        String _toAcc = transfer.get_toAcc();
+
+            Account fromAcc = acm.getAccountByUsername(username);
+            Account toAcc = acm.checkTrasferAccount(_toAcc);
+
+            if(_fromACC == null) throw new SQLException("Sender account not found.");
+            if (_toAcc == null) throw new SQLException("Receiver account not found.");
+
+            if(_fromACC.equals(_toAcc)){
+                throw new SQLException("Cannot transfer to the same account.");
+            }
+
+            if(toAcc == null){
+                fromAcc.withdraw(transfer.get_amount() + 30);
+                toAcc.deposit(transfer.get_amount());
+                acm.saveTransaction(transfer);
+            }else{
+                fromAcc.deposit(transfer.get_amount());
+                toAcc.deposit(transfer.get_amount());
+                acm.updateAccountBalance(fromAcc);
+                acm.saveTransaction(transfer);
+            }
+        }
+}
+
